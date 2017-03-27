c31f011f11c13ae79a77ca7bcf6201acd3b218cf
milesillsley <milesillsley@hotmail.co.uk>
2017-03-27 17:53:18 +0100

    up to 40

---
+++ ex40/03.rb
@@ -0,0 +1,23 @@
+class Song
+
+  def initialize(lyrics)
+    @lyrics = lyrics
+  end
+
+  def sing_me_a_song()
+    @lyrics.each {|line| puts line}
+  end
+end
+
+happy_bday_lyrics = ["Happy birthday to you",
+  "I don't want to get sued",
+  "So I'll stop right there"]
+
+happy_bday = Song.new(happy_bday_lyrics)
+
+  bulls_on_parade = Song.new(["They rally around tha family",
+    "With pockets full of shells"])
+
+happy_bday.sing_me_a_song()
+
+bulls_on_parade.sing_me_a_song()
